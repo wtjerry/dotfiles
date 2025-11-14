9be8b1c4dea3930db7087ce1792c844fc7a98f89
wtjerry <wtjerry123@gmail.com>
2025-11-14 08:36:54 +0100

    conifg gpr to work with different repos

--- bin/git_pr_azure_devops.sh
+++ bin/git_pr_azure_devops.sh
@@ -4,7 +4,10 @@ other_params=$@
 
 work_item=$(git branch --show-current | cut -d/ -f2)
 
+repo_name=$(basename $(git rev-parse --show-toplevel));
+
 az repos pr create \
   --target-branch=main \
   --work-items $work_item \
+  -r $repo_name \
   $other_params
